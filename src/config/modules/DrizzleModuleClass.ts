9a6ff2091a26808613d7616dcac5328fc0498a28
Angel S. Moreno <angelxmoreno@gmail.com>
2025-02-16 14:04:53 -0500

    feat(Database): created DrizzleModule class

---
+++ src/config/modules/DrizzleModuleClass.ts
@@ -0,0 +1,41 @@
+import type { Config as DrizzleKitConfig } from "drizzle-kit";
+import { drizzle } from "drizzle-orm/mysql2";
+
+import * as path from "node:path";
+import type { MySql2Database } from "drizzle-orm/mysql2/driver";
+import type { Logger } from "pino";
+
+export type DatabaseConfig = {
+    url: string;
+};
+
+const dbPath = path.join(__dirname, "..", "..", "db");
+
+export class DrizzleModuleClass {
+    private readonly database: MySql2Database;
+    private readonly logger: Logger;
+    private readonly config: DatabaseConfig;
+    private readonly drizzleKitConfiguration: DrizzleKitConfig;
+
+    constructor(config: DatabaseConfig, logger: Logger) {
+        this.config = config;
+        this.database = drizzle(this.config.url);
+        this.logger = logger;
+
+        this.drizzleKitConfiguration = {
+            dialect: "mysql",
+            out: path.join(dbPath, "migrations"),
+            schema: path.join(dbPath, "schemas"),
+        };
+
+        this.logger.debug(this.drizzleKitConfiguration, "Inferred config");
+    }
+
+    get db(): MySql2Database {
+        return this.database;
+    }
+
+    get drizzleKitConfig(): DrizzleKitConfig {
+        return this.drizzleKitConfiguration;
+    }
+}
