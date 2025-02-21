22185707d57777ed96a2085ec45a5ae16493d40a
Angel S. Moreno <angelxmoreno@gmail.com>
2025-02-21 04:07:45 +0000

    fix(Docker): fixed app container working dir, added more docker scripts

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,8 +1,18 @@
 import * as path from "node:path";
 
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 const rootPath = path.join(__dirname, "..", "..");
 const srcPath = path.join(rootPath, "src");
+const dbPath = path.join(rootPath, "src", "db");
 const unitTestsPath = path.join(rootPath, "tests", "unit");
 const cookRecipesPath = path.join(srcPath, "templates", "cook");
 
-export const appPaths = { root: rootPath, src: srcPath, unitTests: unitTestsPath, cookRecipes: cookRecipesPath };
+export const appPaths = {
+    root: rootPath,
+    src: srcPath,
+    unitTests: unitTestsPath,
+    cookRecipes: cookRecipesPath,
+    db: dbPath,
+};
